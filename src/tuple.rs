0f5d61cdce9306c5f4de778a7a0943a26a7f5311
jimmykiang <castlevanity@gmail.com>
2022-11-05 10:55:35 +0100

    Subtracting a vector from a point.

--- src/tuple.rs
+++ src/tuple.rs
@@ -79,6 +79,14 @@ impl Sub<Point> for Point {
     }
 }
 
+impl Sub<Vector> for Point {
+    type Output = Point;
+
+    fn sub(self, rhs: Vector) -> Self::Output {
+        vec4_sub(self.0, rhs.0).into()
+    }
+}
+
 #[derive(Debug, Clone, Copy)]
 pub struct Vector(Vector4<f64>);
 
@@ -160,5 +168,13 @@ mod tests {
         let p2 = Point::new(5.0, 6.0, 7.0);
 
         assert_eq!(p1 - p2, Vector::new(-2.0, -4.0, -6.0));
+    }
+
+    // Subtracting a vector from a point.
+    #[test]
+    fn substract_vector_from_point() {
+        let p = Point::new(3.0, 2.0, 1.0);
+        let v = Vector::new(5.0, 6.0, 7.0);
+        assert_eq!(p - v, Point::new(-2.0, -4.0, -6.0));
     }
 }
